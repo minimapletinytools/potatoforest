9f7ba70391910d9d7b35fd96aa143f7e3ee3f632
pddla <chippermonky@gmail.com>
2020-01-31 11:48:31 -0800

    potato forest compiles now

--- potatoforest/app/Main.hs
+++ potatoforest/app/Main.hs
@@ -1,6 +1,6 @@
 module Main where
-
-import Lib
+import           Potato.Forest.Types
+import           Relude
 
 main :: IO ()
-main = someFunc
+main = undefined
--- potatoforest/src/Potato/Forest/Types.hs
+++ potatoforest/src/Potato/Forest/Types.hs
@@ -3,10 +3,14 @@ module Potato.Forest.Types (
 
 ) where
 
-import qualified Data.Text as T
-import qualified Data.Map as M
-import qualified Data.Set as S
-import qualified Data.Set.Unicode as S
+import           Relude
+
+-- importing internal gives us access too Map and Set ctors
+import qualified Data.Map.Internal as M
+import qualified Data.Set.Internal as S
+
+import           Data.Maybe
+import qualified Data.Text         as T
 
 -- | parser item
 {-
@@ -20,18 +24,17 @@ data ItemExp = ItemExp BaseItemExp | ExclusiveItemExp BaseItemExp
 -}
 
 
-newtype ItemId = ItemId T.Text
-newtype RecipeId = RecipeId T.Text
+newtype ItemId = ItemId T.Text deriving (Eq, Ord)
+newtype RecipeId = RecipeId T.Text deriving (Eq, Ord)
 
 -- | internal types
 data Item = Item {
-  itemId :: ItemId
+  itemId  :: ItemId
   , title :: T.Text
-  , desc :: T.Text
+  , desc  :: T.Text
   , limit :: Maybe Int
-  , tier :: Maybe Int
+  , tier  :: Maybe Int
 }
-
 
 instance Ord Item where
   (<=) a b = itemId a <= itemId b
@@ -53,11 +56,11 @@ builtin_time = Item {
 type Inventory = M.Map Item Int
 
 data Recipe = Recipe {
-  recipeId :: T.Text
-  , requires :: Inventory
+  recipeId            :: T.Text
+  , requires          :: Inventory
   , exclusiveRequires :: Inventory
-  , inputs :: Inventory
-  , outputs :: Inventory
+  , inputs            :: Inventory
+  , outputs           :: Inventory
 }
 
 instance Ord Recipe where
@@ -75,19 +78,15 @@ type RecipeSet = S.Set Recipe
 type ItemConnections = M.Map Item RecipeSet
 type ItemConnectionsMap = M.Map Item ItemConnections
 
+
 -- | converts a set to map
+-- TODO move this into a different module so we don't have to import internal
 mapSetToMap :: (a -> b) -> S.Set a -> M.Map a b
-mapSetToMap f S.Tip = M.Tip
-mapSetToMap f (S.Bin sz elt ls rs) = M.Bin sz elt (f elt) ls rs
+mapSetToMap f S.Tip                = M.Tip
+mapSetToMap f (S.Bin sz elt ls rs) = M.Bin sz elt (f elt) (mapSetToMap f ls) (mapSetToMap f rs)
 
 findItemConnections :: RecipeSet -> Item -> ItemConnections
 findItemConnections = undefined
-
--- you can delete me now :)
-filterAccumL :: (a -> b -> (a,Bool)) -> a -> [b] -> (a,[b])
-filterAccumL f a bs = (out_a, out_bs) where
-  (out_a, cs) = mapAccumL f a bs
-  out_bs = catMaybes $ zipWith (\a' c' -> if c' then Just a' else Nothing) cs
 
 -- | does not handle forced tiers yet
 -- note that the first pass of this function should completely populate ItemConnectionsMap (unless we do early exit)
@@ -97,12 +96,12 @@ findNextTier ::
   -> ItemSet -- ^ list of items in lower tiers
   -> ItemSet -- ^ all items we want to search (must exclude items in lower tiers)
   -> ItemSet -- ^ returns set of items in next tier
-findNextTier recipes allConns lowerTiers searchItems = filter ffn allConns searchItems where
+findNextTier recipes allConns lowerTiers searchItems = S.filter (ffn allConns) searchItems where
   ffn allConns' item' = case M.lookup item' allConns of
     -- no ItemConnections means it's a tier 0 item
-    Nothing -> if null lowerTiers then True else False
+    Nothing -> null lowerTiers
     -- all required items are contained in lower tiers
-    Just x -> M.foldrWithKey (\k _ acc -> acc && member k lowerTiers) x
+    Just x  -> M.foldrWithKey (\k _ acc -> acc && S.member k lowerTiers) True x
 
 generateTieredItems ::
   ItemSet -- ^ all items
@@ -113,5 +112,6 @@ generateTieredItems items recipes = unfoldr getNextTieredItems (S.empty, items) 
   getNextTieredItems :: (ItemSet, ItemSet) -> Maybe (ItemConnectionsMap, (ItemSet, ItemSet))
   getNextTieredItems (lowerTiers, searchItems) = r where
     r1 = findNextTier recipes allConnections lowerTiers searchItems
-    r2 = (lowerTiers S.∪ r1, searchItems S.∖ r1)
-    r = if S.null searchItems then Nothing else (r1, r2)
+    r2 = (S.union lowerTiers r1, searchItems S.\\ r1)
+    r1c = mapSetToMap (\item -> M.findWithDefault M.empty item allConnections) r1
+    r = if S.null searchItems then Nothing else Just (r1c, r2)
